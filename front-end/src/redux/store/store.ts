e4dd3a2f195601ffa1351a5217148e901e626aa9
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-01-21 15:54:27 +0200

    Redux query was setuped

--- front-end/src/redux/store/store.ts
+++ front-end/src/redux/store/store.ts
@@ -1,11 +1,17 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { api } from '../state/api';
 import { globalSlice } from '../state/globale-slice';
 
 const rootReducer = combineReducers({
 	global: globalSlice.reducer,
+	[api.reducerPath]: api.reducer,
 });
 
 export const store = configureStore({
 	reducer: rootReducer,
+	middleware: (getDefault) => getDefault().concat(api.middleware),
 	devTools: process.env.NODE_ENV !== 'production',
 });
+
+setupListeners(store.dispatch);
