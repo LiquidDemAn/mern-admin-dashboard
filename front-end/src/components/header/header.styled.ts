9dcbe91c358132ad8040e839c03bc42e1695b550
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-01-22 17:27:07 +0200

    Header component for pages was created

---
+++ front-end/src/components/header/header.styled.ts
@@ -0,0 +1,6 @@
+import { styled, Typography } from '@mui/material';
+
+export const Title = styled(Typography)({
+	fontWeight: 700,
+	marginBottom: 5,
+});
---
+++ front-end/src/components/header/header.tsx
@@ -0,0 +1,23 @@
+import { Typography, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
+import { Title } from './header.styled';
+
+type Props = {
+	title: string;
+	subtitle?: string;
+};
+
+export const Header = ({ title, subtitle }: Props) => {
+	const { palette } = useTheme();
+
+	return (
+		<Box>
+			<Title variant='h2' color={palette.secondaryCustom[100]}>
+				{title}
+			</Title>
+			<Typography variant='h5' color={palette.secondaryCustom[300]}>
+				{subtitle}
+			</Typography>
+		</Box>
+	);
+};
