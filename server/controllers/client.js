650c723c564c430569bb78cd16b6eb3db7427cd7
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-01-29 11:16:26 +0200

    GetGeography method was created

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -3,6 +3,7 @@ import ProductStat from '../models/ProductStat.js';
 import Transaction from '../models/Transaction.js';
 import User from '../models/User.js';
 import { generateSort } from '../utils.js';
+import getCountryIso3 from 'country-iso-2-to-3';
 
 export const getProducts = async (req, res) => {
 	try {
@@ -50,7 +51,7 @@ export const getCustomers = async (req, res) => {
 export const getTransactions = async (req, res) => {
 	try {
 		// sort should look like this: {'field': 'userId', 'sort': 'desc'}
-		const { page = 1, pageSize = 20, sort = null, search = '' } = req.query;
+		const { page = 0, pageSize = 20, sort = null, search = '' } = req.query;
 
 		const sortFormatted = Boolean(sort) ? generateSort(sort) : {};
 
@@ -70,6 +71,38 @@ export const getTransactions = async (req, res) => {
 			transactions,
 			total,
 		});
+	} catch (error) {
+		console.log(error);
+
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const getGeography = async (req, res) => {
+	try {
+		const users = await User.find().exec();
+
+		const mappedLocations = users.reduce((acc, { country }) => {
+			const countryISO3 = getCountryIso3(country);
+
+			if (!acc[countryISO3]) {
+				acc[countryISO3] = 0;
+			}
+
+			acc[countryISO3]++;
+
+			return acc;
+		}, {});
+
+		const formattedLocations = Object.entries(mappedLocations).map(
+			([country, count]) => {
+				return { id: country, value: count };
+			}
+		);
+
+		res.json(formattedLocations);
 	} catch (error) {
 		console.log(error);
 
