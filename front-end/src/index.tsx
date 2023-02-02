27fe5edf5d144adaeefeceae6a28fecc2fe5598e
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-02-02 14:38:10 +0200

    BrowserRouter was changed to HashRouter

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,17 +3,17 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	// <React.StrictMode>
-	<BrowserRouter>
+	<HashRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 	// </React.StrictMode>
 );
